08a5ebef44226a5daf7fd2a6ecd937467f738e00
adaskhan <baglanadaskan@gmail.com>
2024-02-08 21:27:40 +0600

    first lesson about formatting string

---
+++ first_lesson.py
@@ -0,0 +1,19 @@
+"""
+ - First lesson
+"""
+
+# 1st method
+name = "Baglan"
+age = 20
+print('Hello, %s'%name)
+
+
+# 2nd method
+print(f"My name is {name}, my age is {age}")
+
+# 3 method
+print("My name is {}, my age is {}".format(name, age))
+print("My name is {n}, my age is {a}".format(a=age, n=name))
+print("My name is {1}, my age is {0}".format(age, name))
+
+
